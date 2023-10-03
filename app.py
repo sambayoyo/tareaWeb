b7574e92b097534e41689991382059b098ccf11d
sambayoyo <csamuert@gmail.com>
2023-10-02 22:58:14 -0500

    models incompletos

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 app = Flask("/")
 
 
-from api.Clientes import ruta_cliente
+from api.Usuarios import ruta_cliente
 from api.Ruta import  ruta_ruta
 from api.Alertas import ruta_alertas
 from api.Community import ruta_community
